2b1ae2a7456a8d4dc36d57a6493caa5d29adf3d7
frrlrbn <farrelrabbani88@gmail.com>
2025-06-30 15:31:45 +0700

    Merapikan frontend Hero & About

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "FWB+ | Event Organizer",
@@ -21,7 +13,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
         {children}
